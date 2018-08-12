6f38486beff8cbaee85bdcdba59751245a437044
Brendon <brendonbarringer@gmail.com>
2018-08-12 18:31:45 -0500

    Finished Memory Game

--- memorygame/src/App.js
+++ memorygame/src/App.js
@@ -1,19 +1,112 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import MemoryCard from "./components/MemoryCard";
+import Wrapper from "./components/Wrapper";
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+import matches from "./clicky.json";
+import "./App.css";
+
+
+let score = 0;
+let topScore = 0;
+let message = "Click an image to begin!";
 
 class App extends Component {
+  state = {
+    matches,
+    score,
+    topScore,
+    message
+  }
+
+  setClicked = id => {
+
+    const matches = this.state.matches;
+
+    const clickedMatch = matches.filter(match => match.id === id);
+
+
+    if (clickedMatch[0].clicked){
+
+        score = 0;
+        message = "Reset"
+
+        for (let i = 0 ; i < matches.length ; i++){
+            matches[i].clicked = false;
+        }
+
+        this.setState({message});
+        this.setState({score});
+        this.setState({matches});
+
+    } else if (score < 11) {
+
+        clickedMatch[0].clicked = true;
+
+        score++;
+        
+        message = "";
+
+        if (score > topScore){
+            topScore = score;
+            this.setState({topScore});
+        }
+
+        matches.sort(function(a, b){return 0.5 - Math.random()});
+
+        this.setState({matches});
+        this.setState({score});
+        this.setState({message});
+    } else {
+
+        clickedMatch[0].clicked = true;
+
+        score = 0;
+
+        message = "You Win!";
+        topScore = 12;
+        this.setState({topScore});
+        
+        for (let i = 0 ; i < matches.length ; i++){
+            matches[i].clicked = false;
+        }
+
+        matches.sort(function(a, b){return 0.5 - Math.random()});
+
+        this.setState({ matches });
+        this.setState({score});
+        this.setState({message});
+
+    }
+};
+
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Wrapper>
+        <Navbar>
+        <ul>
+            <li>ClickyGame</li>
+            <li>{this.state.message}</li>
+            <li className="scoreSummary">Correct Guesses: {this.state.score} | Best Score: {this.state.topScore}</li>
+          </ul>
+        </Navbar>
+
+        <Header>
+          <h3 className="headerText">Win the game by clicking each picture only once!</h3>
+        </Header>
+
+       
+        {this.state.matches.map(match => (
+          <MemoryCard
+            setClicked={this.setClicked}
+            id={match.id}
+            key={match.id}
+            name={match.name}
+            image={match.image}
+          />
+        ))}
+      </Wrapper>
     );
   }
 }
---
+++ memorygame/src/components/Navbar/Navbar.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./Navbar.css";
+
+
+const Navbar = props => <nav className="navbar col-md-12">{props.children}</nav>;
+
+export default Navbar;
+
+
